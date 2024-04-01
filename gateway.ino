0746379d50c4642c520ebf796c2de07b047bd08d
duongg.hoangg <dinhhoangduong2242003@gmail.com>
2024-04-01 22:56:19 +0700

    Update gateway.ino
    adding push data from esp32 to firebase

--- gateway.ino
+++ gateway.ino
@@ -1,33 +1,57 @@
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 #include <LoRa.h>
+#include <LiquidCrystal_I2C.h>
+#include <FirebaseESP32.h>
+#include "firebaseConnectionInfo.h" // File chứa thông tin kết nối Firebase
+
+#define FIREBASE_PROJECT_ID ""
+#define FIREBASE_DATABASE_URL "https://" FIREBASE_PROJECT_ID ".firebaseio.com"
+#define FIREBASE_SECRET "" // Secret key để truy cập dữ liệu Firebase
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+FirebaseData firebaseData;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
+
+  lcd.begin();
+  lcd.backlight();
+  lcd.clear();
 
   if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
 
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
-  lcd.print("LoRa Gateway");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Firebase.begin(FIREBASE_DATABASE_URL, FIREBASE_SECRET);
 }
 
 void loop() {
-  if (LoRa.parsePacket()) {
-    float temperature = LoRa.parseFloat();
-    Serial.print("Received temperature: ");
-    Serial.println(temperature);
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    while (LoRa.available()) {
+      String sensorData = LoRa.readString();
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Received: ");
+      lcd.setCursor(0, 1);
+      lcd.print(sensorData);
 
-    lcd.clear();
-    lcd.print("Temp: ");
-    lcd.print(temperature);
-    lcd.print("C");
+      if (Firebase.pushString(firebaseData, "/sensorData", sensorData)) {
+        Serial.println("Data pushed to Firebase!");
+      } else {
+        Serial.println("Pushing data to Firebase failed!");
+        Serial.println("Reason: " + firebaseData.errorReason());
+      }
+
+      delay(3000);
+    }
   }
 }

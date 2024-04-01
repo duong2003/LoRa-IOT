b422ddee0d6d5e4a7db021acf195c0f86c436128
duongg.hoangg <dinhhoangduong2242003@gmail.com>
2024-04-01 23:34:59 +0700

    Update DS18B20 node.ino
    adding comment

--- DS18B20 node.ino
+++ DS18B20 node.ino
@@ -9,26 +9,26 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
+  Serial.begin(9600); // Khởi tạo giao tiếp serial baudrate 9600 
+  while (!Serial); // Đợi cho đến khi serial đã sẵn sàng
 
-  if (!LoRa.begin(433E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
+  if (!LoRa.begin(433E6)) { // Khởi tạo module LoRa ở tần số 433MHz
+    Serial.println("Starting LoRa failed!"); // In ra serial thông báo lỗi nếu khởi tạo LoRa thất bại
+    while (1); // Dừng chương trình
   }
 
-  sensors.begin();
+  sensors.begin(); // Khởi tạo cảm biến DS18B20
 }
 
 void loop() {
-  sensors.requestTemperatures();
-  float temperature = sensors.getTempCByIndex(0);
+  sensors.requestTemperatures(); // Gửi yêu cầu để cảm biến nhiệt độ đo nhiệt độ
+  float temperature = sensors.getTempCByIndex(0); // Lấy giá trị nhiệt độ từ cảm biến và lưu vào biến temperature
 
-  String data = "Temperature: " + String(temperature) + " C";
-  LoRa.beginPacket();
-  LoRa.print(data);
-  LoRa.endPacket();
+  String data = "Temperature: " + String(temperature) + " C"; // Tạo một chuỗi dữ liệu chứa thông tin về nhiệt độ
+  LoRa.beginPacket(); // Bắt đầu gửi gói tin LoRa
+  LoRa.print(data); // Gửi dữ liệu thông qua LoRa
+  LoRa.endPacket(); // Kết thúc gửi gói tin LoRa
 
-  Serial.println("Sent: " + data);
-  delay(5000); // Gửi dữ liệu mỗi 5 giây
+  Serial.println("Sent: " + data); // In ra serial thông tin về dữ liệu đã gửi
+  delay(5000); // Dừng chương trình trong 5 giây trước khi lặp lại quá trình gửi dữ liệu
 }

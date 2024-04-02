12ee1d195eb6aa7ecc9913c14d23c2b717a4274c
duongg.hoangg <dinhhoangduong2242003@gmail.com>
2024-04-02 14:55:16 +0700

    Update Soil Moisture Sensor node.ino
    adding define pin

--- Soil Moisture Sensor node.ino
+++ Soil Moisture Sensor node.ino
@@ -1,10 +1,10 @@
 #include <Wire.h>
 #include <LoRa.h>
 
-#define SOIL_MOISTURE_PIN 5 // Chân kết nối với Soil Moisture Sensor
-#define LORA_SS 18 // Define the chip select pin for the LoRa module.
-#define LORA_RST 14 // Define the reset pin for the LoRa module.
-#define LORA_DI0 26 // Define the DIO0 pin for interrupt handling.
+#define SOIL_MOISTURE_PIN 5 // Pin connected to Soil Moisture Sensor
+#define LORA_CS_PIN      18
+#define LORA_RST_PIN     14
+#define LORA_IRQ_PIN     26
 
 void setup() {
   Serial.begin(9600);
@@ -16,6 +16,8 @@ void setup() {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
+
+  LoRa.setPins(LORA_CS_PIN, LORA_RST_PIN, LORA_IRQ_PIN);
 }
 
 void loop() {
@@ -27,5 +29,5 @@ void loop() {
   LoRa.endPacket();
 
   Serial.println("Sent: " + data);
-  delay(5000); // Gửi dữ liệu mỗi 5 giây
+  delay(5000); // Send data every 5 seconds
 }

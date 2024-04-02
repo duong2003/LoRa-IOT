03673d949d1a9582fa8d2bcf3347b2d332ce85d3
duongg.hoangg <dinhhoangduong2242003@gmail.com>
2024-04-02 14:31:54 +0700

    Update Soil Moisture Sensor node.ino
    adding define pin for sx1278

--- Soil Moisture Sensor node.ino
+++ Soil Moisture Sensor node.ino
@@ -2,6 +2,9 @@
 #include <LoRa.h>
 
 #define SOIL_MOISTURE_PIN 5 // Chân kết nối với Soil Moisture Sensor
+#define LORA_SS 18 // Define the chip select pin for the LoRa module.
+#define LORA_RST 14 // Define the reset pin for the LoRa module.
+#define LORA_DI0 26 // Define the DIO0 pin for interrupt handling.
 
 void setup() {
   Serial.begin(9600);

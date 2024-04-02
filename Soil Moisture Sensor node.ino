31064fa8be1529eb767921b0794f49e5d63ab724
duongg.hoangg <dinhhoangduong2242003@gmail.com>
2024-04-02 15:00:17 +0700

    Update Soil Moisture Sensor node.ino
    define pin

--- Soil Moisture Sensor node.ino
+++ Soil Moisture Sensor node.ino
@@ -2,32 +2,32 @@
 #include <LoRa.h>
 
 #define SOIL_MOISTURE_PIN 5 // Pin connected to Soil Moisture Sensor
-#define LORA_CS_PIN      18
-#define LORA_RST_PIN     14
-#define LORA_IRQ_PIN     26
+#define LORA_CS_PIN      18  // LoRa chip select pin
+#define LORA_RST_PIN     14  // LoRa reset pin
+#define LORA_IRQ_PIN     26  // LoRa interrupt pin
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
+  Serial.begin(9600); // Initialize serial communication
+  while (!Serial); // Wait until serial port is ready
 
-  pinMode(SOIL_MOISTURE_PIN, INPUT);
+  pinMode(SOIL_MOISTURE_PIN, INPUT); // Set Soil Moisture Sensor pin as input
 
-  if (!LoRa.begin(433E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
+  if (!LoRa.begin(433E6)) { // Initialize LoRa module at 433MHz frequency
+    Serial.println("Starting LoRa failed!"); // Print error message if LoRa initialization fails
+    while (1); // Halt the program
   }
 
-  LoRa.setPins(LORA_CS_PIN, LORA_RST_PIN, LORA_IRQ_PIN);
+  LoRa.setPins(LORA_CS_PIN, LORA_RST_PIN, LORA_IRQ_PIN); // Set LoRa pins
 }
 
 void loop() {
-  int soilMoisture = analogRead(SOIL_MOISTURE_PIN);
+  int soilMoisture = analogRead(SOIL_MOISTURE_PIN); // Read soil moisture value from sensor
   
-  String data = "Soil Moisture: " + String(soilMoisture);
-  LoRa.beginPacket();
-  LoRa.print(data);
-  LoRa.endPacket();
+  String data = "Soil Moisture: " + String(soilMoisture); // Create data string containing soil moisture value
+  LoRa.beginPacket(); // Start sending LoRa packet
+  LoRa.print(data); // Send data via LoRa
+  LoRa.endPacket(); // End LoRa packet transmission
 
-  Serial.println("Sent: " + data);
-  delay(5000); // Send data every 5 seconds
+  Serial.println("Sent: " + data); // Print sent data to serial monitor
+  delay(5000); // Wait for 5 seconds before sending next data
 }
